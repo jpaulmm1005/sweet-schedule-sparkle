db6e67b754af6e838d908110589ac6334061c891
Jean Paul <jpaulmm1005@gmail.com>
2025-08-18 21:56:32 -0500

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "./" : "/",
+  base: mode === "production" ? "/sweet-schedule-sparkle/" : "/",
   server: {
     host: "::",
     port: 8080,
